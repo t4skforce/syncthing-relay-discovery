f0dedcc4932213bd1a83ceed5581cda9dc96512c
root <t4skforce1337@gmail.com>
2018-06-30 12:47:16 +0200

    #2 removed stats param

--- start_discovery.sh
+++ start_discovery.sh
@@ -4,4 +4,4 @@ do
   echo "waiting for certificates."
   sleep 1
 done
-${USER_HOME}/server/discosrv -listen=":${DISCO_PORT}" -stats-file="/home/discosrv/stats" -db-dir="${USER_HOME}/db/discosrv.db" -cert="${USER_HOME}/certs/cert.pem" -key="${USER_HOME}/certs/key.pem" ${DISCO_OPTS}
+${USER_HOME}/server/discosrv -listen=":${DISCO_PORT}" -db-dir="${USER_HOME}/db/discosrv.db" -cert="${USER_HOME}/certs/cert.pem" -key="${USER_HOME}/certs/key.pem" ${DISCO_OPTS}
